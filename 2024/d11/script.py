0c49f073de9980e069b7b51e19d2d469bf765c7f
Brede Eder Murberg <brede@murberg.no>
2024-12-13 11:43:14 +0100

    day 11 solved...

--- 2024/d11/script.py
+++ 2024/d11/script.py
@@ -1,50 +1,29 @@
-from functools import reduce
-from operator import iconcat
-
 INPUT_FILE = "input.txt"
 
-BLINKS = 6
+BLINKS = 75
 
 cache = {}
 
-def apply_rules(n: str):
-    if not cache.get(n):
-        if len(n) % 2 == 0:
-            cache[n] = (n[:len(n)//2], n[len(n)//2:])
-        else:
-            cache[n] = str(int(n)*2024)
-
-    return cache[n]
-
-def _recurse(blinks: int, n: str):
-    if blinks >= BLINKS:
+def _recurse(blinks: int, n: int):
+    if blinks == BLINKS:
         return 1
 
     if (n, blinks) in cache:
         return cache[(n, blinks)]
     
-
-    if n == "0":
-        cache[(n, blinks)] = _recurse(blinks+1, "1")
-    elif (l := len(n)) % 2 == 0:
-        cache[(n, blinks)] = _recurse(blinks+1, n[:l//2]) + _recurse(blinks+1, n[l//2:])
+    if n == 0:
+        cache[(n, blinks)] = _recurse(blinks+1, 1)
+    elif (l := len(str(n))) % 2 == 0:
+        cache[(n, blinks)] = _recurse(blinks+1, int(str(n)[:l//2])) + _recurse(blinks+1, int(str(n)[l//2:]))
     else:
-        cache[(n, blinks)] = _recurse(blinks+1, str(n*2024))
+        cache[(n, blinks)] = _recurse(blinks+1, n*2024)
     return cache[(n, blinks)]
 
-    # new_num = apply_rules(num)
-    # if type(new_num) == tuple:
-    #     return _recurse(blinks+1, new_num[0]) + _recurse(blinks+1, new_num[1])
-    # else:
-    #     return _recurse(blinks+1, new_num)
-
-
-def part2(stones: list[str]):
+def part2(stones: list[int]):
     res = 0
 
-    for i, stone in enumerate(stones, 1):
+    for stone in stones:
         res += _recurse(0, stone)
-        print(f"{12.5 * i:.1f}% done...")
     
     print("Part 2:")
     print(f"{res} stones after {BLINKS} blinks.")
@@ -54,11 +33,8 @@ def part2(stones: list[str]):
 def main():
     
     with open(INPUT_FILE, "r") as f:
-        input_list = f.read().split()
-    
-    # part1(input_list)
+        input_list = [int(n) for n in f.read().split()]
     part2(input_list)
-
 
 if __name__ == '__main__':
     main()
