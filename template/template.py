7e3a280d5659fb6aadc1488447feb24e99d07f6c
Brede Eder Murberg <brede@murberg.no>
2025-12-08 09:50:16 +0100

    update template

--- template/template.py
+++ template/template.py
@@ -1,18 +1,25 @@
-
 INPUT_FILE = "input.txt"
+# INPUT_FILE = "test_input.txt"
+
 
 def part1():
-    pass
+
+    print(f"Part 1: ")
+
 
 def part2():
-    pass
+
+    print(f"Part 2: ")
+
 
 def main():
-    
     with open(INPUT_FILE, "r") as f:
         input_string = f.read()
 
+    # part1()
+    # part2()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
+
